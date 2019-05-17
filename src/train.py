1b3b5728cb12125c06bc5e5360186e100888b45e
danil328 <danil328@yandex.ru>
2019-05-17 11:44:50 +0300

    add score to validation

--- src/train.py
+++ src/train.py
@@ -10,58 +10,59 @@ from torchvision.models import resnet34, resnet101
 from Dataset.id_rnd_dataset import IDRND_dataset
 from model.network import Model
 from utils.loss import FocalLoss
-from utils.metrics import idrnd_score_pytorch
+from utils.metrics import idrnd_score_pytorch, far_score, frr_score
 
 if __name__ == '__main__':
-	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-	train_dataset = IDRND_dataset(mode = 'train')
-	train_loader = DataLoader(train_dataset, batch_size = 256, shuffle = True, num_workers = 6, drop_last = True)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    train_dataset = IDRND_dataset(mode='train')
+    train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=6, drop_last=True)
 
-	val_dataset = IDRND_dataset(mode = 'val')
-	val_loader = DataLoader(val_dataset, batch_size = 256, shuffle = False, num_workers = 2, drop_last = True)
+    val_dataset = IDRND_dataset(mode='val')
+    val_loader = DataLoader(val_dataset, batch_size=256, shuffle=False, num_workers=2, drop_last=True)
 
-	model = Model(base_model = resnet34(pretrained = True))
+    model = Model(base_model=resnet101(pretrained=True))
 
-	dataowner = DataOwner(train_loader, val_loader, None)
-	criterion = torch.nn.BCELoss()
-	# criterion = FocalLoss()
+    dataowner = DataOwner(train_loader, val_loader, None)
+    criterion = torch.nn.BCELoss()
+    # criterion = FocalLoss()
 
-	try:
-		shutil.rmtree('../output/logs', ignore_errors = True)
-	except Exception:
-		pass
-	os.mkdir('../output/logs')
+    try:
+        shutil.rmtree('../output/logs', ignore_errors=True)
+    except Exception:
+        pass
+    os.mkdir('../output/logs')
 
-	keker = Keker(model = model,
-				  dataowner = dataowner,
-				  criterion = criterion,
-				  target_key = "label",
-				  metrics = {"acc": accuracy, "roc_auc": roc_auc, "idrnd_score": idrnd_score_pytorch},
-				  opt = torch.optim.Adam,
-				  device = device,
-				  opt_params = {"weight_decay": 1e-5})
+    keker = Keker(model=model,
+                  dataowner=dataowner,
+                  criterion=criterion,
+                  target_key="label",
+                  metrics={"acc": accuracy, "roc_auc": roc_auc, "idrnd_score": idrnd_score_pytorch, "FAR": far_score,
+                           "FRR": frr_score},
+                  opt=torch.optim.Adam,
+                  device=device,
+                  opt_params={"weight_decay": 1e-5})
 
-	# find LR
-	# keker.kek_lr(final_lr = 0.1, logdir = "../output/lr_find_log")
+    # find LR
+    # keker.kek_lr(final_lr = 0.1, logdir = "../output/lr_find_log")
 
-	keker.kek(lr = 1e-3,
-			  epochs = 10,
-			  opt = torch.optim.Adam,
-			  opt_params = {"weight_decay": 1e-5},
-			  sched = torch.optim.lr_scheduler.StepLR,
-			  sched_params = {"step_size": 1, "gamma": 0.9},
-			  logdir = "../output/logs",
-			  cp_saver_params = {
-				  "savedir": "../output/models/",
-				  "metric": "roc_auc",
-				  "n_best": 3,
-				  "prefix": "kek",
-				  "mode": "max"},
-			  early_stop_params = {
-				  "patience": 3,
-				  "metric": "roc_auc",
-				  "mode": "max",
-				  "min_delta": 0
-			  })
-	# saving
-	keker.save("../output/kek")
+    keker.kek(lr=1e-3,
+              epochs=10,
+              opt=torch.optim.Adam,
+              opt_params={"weight_decay": 1e-5},
+              sched=torch.optim.lr_scheduler.StepLR,
+              sched_params={"step_size": 1, "gamma": 0.9},
+              logdir="../output/logs",
+              cp_saver_params={
+                  "savedir": "../output/models/",
+                  "metric": "roc_auc",
+                  "n_best": 3,
+                  "prefix": "kek",
+                  "mode": "max"},
+              early_stop_params={
+                  "patience": 3,
+                  "metric": "roc_auc",
+                  "mode": "max",
+                  "min_delta": 0
+              })
+    # saving
+    keker.save("../output/kek")
