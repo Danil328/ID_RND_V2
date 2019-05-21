621d70feec85a7ff03af8ddac8a363044b01f705
Danil328 <Danil328@yandex.ru>
2019-05-21 21:07:21 +0300

    add DenchikNet

--- src/train3D.py
+++ src/train3D.py
@@ -11,6 +11,7 @@ from torchsummary import summary
 from Dataset.id_rnd_dataset import IDRND_3D_dataset
 from model.D3_resnet import resnet34
 from model.network import Model
+from model.DenchikNet import DenchikModel
 from src.tools import str2bool
 from utils.loss import FocalLoss
 from utils.metrics import idrnd_score_pytorch, far_score, frr_score, bce_accuracy, search_threshold
@@ -28,57 +29,57 @@ if __name__ == '__main__':
 	val_dataset = IDRND_3D_dataset(mode=config['mode'].replace('train', 'val'), use_face_detection=str2bool(config['use_face_detection']))
 	val_loader = DataLoader(val_dataset, batch_size=config['batch_size'], shuffle=True, num_workers=4, drop_last=False)
 
-	model = resnet34()
+	model = DenchikModel(n_features=32)
 	summary(model, (3, 5, 224, 224), device="cpu")
 
-	dataowner = DataOwner(train_loader, val_loader, None)
-	criterion = FocalLoss()
-
-	shutil.rmtree('../output/logs')
-	os.mkdir('../output/logs')
-
-	keker = Keker(model=model,
-				  dataowner=dataowner,
-				  criterion=criterion,
-				  target_key="label",
-				  metrics={"acc": bce_accuracy, "idrnd_score": idrnd_score_pytorch, "FAR": far_score,
-						   # "roc_auc": roc_auc,
-						   "FRR": frr_score},
-				  opt=torch.optim.Adam,
-				  device=device,
-				  opt_params={"weight_decay": 1e-5})
-
-	# find LR
-	# keker.kek_lr(final_lr = 0.1, logdir = "../output/lr_find_log")
-
-	keker.kek(lr=config['learning_rate'],
-			  epochs=config['number_epochs'],
-			  opt=torch.optim.Adam,
-			  opt_params={"weight_decay": config['weight_decay']},
-			  # sched=CyclicCosAnnealingLR,
-			  sched=ExponentialLR,
-			  # sched_params={"milestones": [12, 29], "eta_min": 1e-8},
-			  sched_params={"gamma": 0.9},
-			  logdir="../output/logs",
-			  cp_saver_params={
-				  "savedir": "../output/models/",
-				  "metric": "idrnd_score",
-				  "n_best": 3,
-				  "prefix": "kek",
-				  "mode": "min"},
-			  # early_stop_params={
-			  #     "patience": 5,
-			  #     "metric": "idrnd_score",
-			  #     "mode": "min",
-			  #     "min_delta": 0}
-			  )
-	# saving
-	keker.save("../output/kek")
-
-	keker.load("../output/kek")
-
-	# Search best threshold
-	predict = keker.predict_loader(val_loader)
-	labels = val_dataset.val_labels
-	best_idrnd_score, threshold = search_threshold(labels, predict)
-	print(f"Best score - {best_idrnd_score}\nBest threshold - {threshold}")
+	# dataowner = DataOwner(train_loader, val_loader, None)
+	# criterion = FocalLoss()
+	#
+	# shutil.rmtree('../output/logs')
+	# os.mkdir('../output/logs')
+	#
+	# keker = Keker(model=model,
+	# 			  dataowner=dataowner,
+	# 			  criterion=criterion,
+	# 			  target_key="label",
+	# 			  metrics={"acc": bce_accuracy, "idrnd_score": idrnd_score_pytorch, "FAR": far_score,
+	# 					   # "roc_auc": roc_auc,
+	# 					   "FRR": frr_score},
+	# 			  opt=torch.optim.Adam,
+	# 			  device=device,
+	# 			  opt_params={"weight_decay": 1e-5})
+	#
+	# # find LR
+	# # keker.kek_lr(final_lr = 0.1, logdir = "../output/lr_find_log")
+	#
+	# keker.kek(lr=config['learning_rate'],
+	# 		  epochs=config['number_epochs'],
+	# 		  opt=torch.optim.Adam,
+	# 		  opt_params={"weight_decay": config['weight_decay']},
+	# 		  # sched=CyclicCosAnnealingLR,
+	# 		  sched=ExponentialLR,
+	# 		  # sched_params={"milestones": [12, 29], "eta_min": 1e-8},
+	# 		  sched_params={"gamma": 0.9},
+	# 		  logdir="../output/logs",
+	# 		  cp_saver_params={
+	# 			  "savedir": "../output/models/",
+	# 			  "metric": "idrnd_score",
+	# 			  "n_best": 3,
+	# 			  "prefix": "kek",
+	# 			  "mode": "min"},
+	# 		  # early_stop_params={
+	# 		  #     "patience": 5,
+	# 		  #     "metric": "idrnd_score",
+	# 		  #     "mode": "min",
+	# 		  #     "min_delta": 0}
+	# 		  )
+	# # saving
+	# keker.save("../output/kek")
+	#
+	# keker.load("../output/kek")
+	#
+	# # Search best threshold
+	# predict = keker.predict_loader(val_loader)
+	# labels = val_dataset.val_labels
+	# best_idrnd_score, threshold = search_threshold(labels, predict)
+	# print(f"Best score - {best_idrnd_score}\nBest threshold - {threshold}")
